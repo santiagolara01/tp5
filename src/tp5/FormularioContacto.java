ca61460c422fee3be1fb2e204103ee74029c1dd7
Gabriel Jara <95397129+Gabriel-Jara@users.noreply.github.com>
2024-09-18 14:18:15 -0300

    Programación de botones
    Se agregó el botón descartar.
    
    Se programaron las funciones de los botones:
    -Nuevo
    -Salir
    -Buscar
    -Descartar
    -Guardar (Falta probar y terminar)

--- src/tp5/FormularioContacto.java
+++ src/tp5/FormularioContacto.java
@@ -1,19 +1,29 @@
-
 package tp5;
 
 import java.util.TreeMap;
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
 
 /**
  * @author Santiago Lara
  */
 public class FormularioContacto extends javax.swing.JFrame {
-    
+
     private DirectorioTelefonico directorio;
+    DefaultListModel modelo = new DefaultListModel();
+
     public FormularioContacto(DirectorioTelefonico directorio) {
         this.directorio = directorio;
         initComponents();
+        btn_guardar.setEnabled(false);
+        btn_borrar.setEnabled(false);
+        txt_dni.setEnabled(false);
+        txt_nombre.setEnabled(false);
+        txt_direccion.setEnabled(false);
+        lista_resultados.setModel(modelo);
+        lista_resultados.setVisible(false);
+        btn_descartar.setEnabled(false);
     }
-
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -44,6 +54,7 @@ public class FormularioContacto extends javax.swing.JFrame {
         lbl_lista = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         lista_resultados = new javax.swing.JList<>();
+        btn_descartar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -59,73 +70,117 @@ public class FormularioContacto extends javax.swing.JFrame {
 
         jLabel6.setText("Formulario de Contacto");
 
+        txt_apellido.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_apellidoKeyReleased(evt);
+            }
+        });
+
+        txt_ciudad.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_ciudadKeyReleased(evt);
+            }
+        });
+
         btn_buscar.setText("Buscar");
+        btn_buscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_buscarActionPerformed(evt);
+            }
+        });
 
         jLabel7.setText("Telefono:");
 
+        txt_telefono.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_telefonoKeyReleased(evt);
+            }
+        });
+
         btn_nuevo.setText("Nuevo");
+        btn_nuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_nuevoActionPerformed(evt);
+            }
+        });
 
         btn_guardar.setText("Guardar");
+        btn_guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_guardarActionPerformed(evt);
+            }
+        });
 
         btn_borrar.setText("Borrar");
 
         btn_salir.setText("Salir");
+        btn_salir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_salirActionPerformed(evt);
+            }
+        });
 
         btn_buscarTelefono.setText("Buscar teléfonos");
 
         btn_buscarContact.setText("Buscar contactos");
 
         lbl_lista.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lbl_lista.setText("Lista ");
 
         jScrollPane1.setViewportView(lista_resultados);
+
+        btn_descartar.setText("Descartar");
+        btn_descartar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_descartarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(btn_nuevo)
-                        .addGap(69, 69, 69)
-                        .addComponent(btn_guardar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn_borrar))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(359, 359, 359)
-                        .addComponent(btn_salir)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jSeparator1)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel7))
-                        .addGap(54, 54, 54)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                            .addComponent(txt_dni)
-                            .addComponent(txt_nombre)
-                            .addComponent(txt_apellido)
-                            .addComponent(txt_direccion)
-                            .addComponent(txt_ciudad)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txt_telefono)
-                            .addComponent(lbl_lista, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel6))
-                        .addGap(18, 18, 18)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel7))
+                                .addGap(54, 54, 54)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                                    .addComponent(txt_dni)
+                                    .addComponent(txt_nombre)
+                                    .addComponent(txt_apellido)
+                                    .addComponent(txt_direccion)
+                                    .addComponent(txt_ciudad)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txt_telefono)
+                                    .addComponent(lbl_lista, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel6))
+                                .addGap(18, 18, 18))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addComponent(btn_nuevo)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btn_descartar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btn_guardar)
+                                .addGap(18, 18, 18)
+                                .addComponent(btn_borrar)
+                                .addGap(69, 69, 69)))
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addComponent(btn_buscarTelefono, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(btn_buscarContact, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addComponent(btn_buscar))
+                            .addComponent(btn_buscar)
+                            .addComponent(btn_salir))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
@@ -173,7 +228,8 @@ public class FormularioContacto extends javax.swing.JFrame {
                     .addComponent(btn_nuevo)
                     .addComponent(btn_guardar)
                     .addComponent(btn_borrar)
-                    .addComponent(btn_salir))
+                    .addComponent(btn_salir)
+                    .addComponent(btn_descartar))
                 .addContainerGap())
         );
 
@@ -196,13 +252,133 @@ public class FormularioContacto extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-   
+    private void btn_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_salirActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_btn_salirActionPerformed
+
+    private void btn_buscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_buscarActionPerformed
+        try {
+            Long telefono = Long.parseLong(txt_telefono.getText());
+            if (txt_telefono.getText().length() == 10) {
+                Cliente cliente = directorio.getListaDeContactos().get(telefono);
+//                System.out.println(directorio.getListaDeContactos()); //Para controlar por consola la lista obtenida
+//                System.out.println(telefono); // Para controlar por consola el número ingresado
+//                System.out.println(cliente);  //Para controlar por consola el cliente obtenido
+                txt_dni.setText(String.valueOf(cliente.dni));
+                txt_nombre.setText(cliente.getNombre());
+                txt_apellido.setText(cliente.getApellido());
+                txt_ciudad.setText(cliente.getCiudad());
+                txt_direccion.setText(cliente.getDireccion());
+                btn_guardar.setEnabled(false);
+                btn_borrar.setEnabled(true);
+                txt_dni.setEnabled(false);
+                txt_nombre.setEnabled(false);
+                txt_direccion.setEnabled(false);
+                lista_resultados.setVisible(false);
+            } else {
+                JOptionPane.showMessageDialog(rootPane, "Debe ingresar un número de 10 dígitos");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(rootPane, "Debe ingresar sólo números");
+        } catch (NullPointerException e) {
+            JOptionPane.showMessageDialog(rootPane, "No existe un contacto registrado con el número indicado");
+        }
+    }//GEN-LAST:event_btn_buscarActionPerformed
+
+    private void btn_nuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_nuevoActionPerformed
+        btn_guardar.setEnabled(true);
+        btn_borrar.setEnabled(false);
+        txt_dni.setEnabled(true);
+        txt_nombre.setEnabled(true);
+        txt_direccion.setEnabled(true);
+        btn_buscarContact.setEnabled(false);
+        btn_buscarTelefono.setEnabled(false);
+        btn_buscar.setEnabled(false);
+        btn_descartar.setEnabled(true);
+        lista_resultados.setVisible(false);
+        btn_nuevo.setEnabled(false);
+    }//GEN-LAST:event_btn_nuevoActionPerformed
+
+    private void btn_descartarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_descartarActionPerformed
+        btn_guardar.setEnabled(false);
+        btn_borrar.setEnabled(false);
+        btn_buscarContact.setEnabled(true);
+        btn_buscarTelefono.setEnabled(true);
+        btn_buscar.setEnabled(true);
+        txt_dni.setEnabled(false);
+        txt_nombre.setEnabled(false);
+        txt_direccion.setEnabled(false);
+        lista_resultados.setVisible(false);
+    }//GEN-LAST:event_btn_descartarActionPerformed
+
+    private void txt_telefonoKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_telefonoKeyReleased
+        txt_dni.setText(String.valueOf(""));
+        txt_nombre.setText("");
+        txt_apellido.setText("");
+        txt_ciudad.setText("");
+        txt_direccion.setText("");
+    }//GEN-LAST:event_txt_telefonoKeyReleased
+
+    private void txt_ciudadKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_ciudadKeyReleased
+        txt_dni.setText(String.valueOf(""));
+        txt_nombre.setText("");
+        txt_apellido.setText("");
+        txt_direccion.setText("");
+        txt_telefono.setText("");
+    }//GEN-LAST:event_txt_ciudadKeyReleased
+
+    private void txt_apellidoKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_apellidoKeyReleased
+        txt_dni.setText(String.valueOf(""));
+        txt_nombre.setText("");
+        txt_ciudad.setText("");
+        txt_direccion.setText("");
+        txt_telefono.setText("");
+    }//GEN-LAST:event_txt_apellidoKeyReleased
+
+    private void btn_guardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_guardarActionPerformed
+        if (verificarCampos()) {
+            int dni = Integer.parseInt(txt_dni.getText());
+            String nombre = txt_nombre.getText();
+            String apellido = txt_apellido.getText();
+            String ciudad = txt_ciudad.getText();
+            String direccion = txt_direccion.getText();
+            Long telefono = Long.parseLong(txt_telefono.getText());
+            Cliente cliente = new Cliente(dni, nombre, apellido, ciudad, direccion);
+            directorio.agregarContacto(telefono, cliente);
+        }
+    }//GEN-LAST:event_btn_guardarActionPerformed
+
+    public boolean verificarCampos() {
+        boolean dni = true;
+        boolean telefono = true;
+        try {
+            if (txt_dni.getText().length() != 8) {
+                dni = false;
+            } else {
+                JOptionPane.showMessageDialog(rootPane, "Debe ingresar un DNI de 8 dígitos");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(rootPane, "Debe ingresar sólo números para el DNI");
+        }
+        try {
+            if (txt_telefono.getText().length() != 10) {
+                telefono = false;
+            } else {
+                JOptionPane.showMessageDialog(rootPane, "Debe ingresar un teléfono de 10 dígitos");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(rootPane, "Debe ingresar sólo números para el teléfono");
+        }
+
+        return txt_nombre.getText().equals("") && txt_apellido.getText().equals("") && txt_ciudad.getText().equals("") && txt_direccion.getText().equals("") && dni && telefono;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_borrar;
     private javax.swing.JButton btn_buscar;
     private javax.swing.JButton btn_buscarContact;
     private javax.swing.JButton btn_buscarTelefono;
+    private javax.swing.JButton btn_descartar;
     private javax.swing.JButton btn_guardar;
     private javax.swing.JButton btn_nuevo;
     private javax.swing.JButton btn_salir;
